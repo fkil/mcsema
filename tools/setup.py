a9ab12d2896df42e45c67b94cb35361b4a537eaf
Fabian Kilger <kilger@sec.in.tum.de>
2021-10-15 15:21:17 +0200

    ghidra specific Dockerfile
    Signed-off-by: Fabian Kilger <kilger@sec.in.tum.de>

--- tools/setup.py
+++ tools/setup.py
@@ -29,7 +29,7 @@ setup(name="mcsema-disass",
       author="Trail of Bits",
       author_email="mcsema@trailofbits.com",
       license='AGPLv3',
-      packages=['mcsema_disass', 'mcsema_disass.ida7', 'mcsema_disass.defs'],
+      packages=['mcsema_disass', 'mcsema_disass.ida7', 'mcsema_disass.defs', 'mcsema_disass.ghidra'],
       install_requires=['protobuf==3.2.0', 'python-magic'],
       package_data={
         "mcsema_disass.defs": ["linux.txt", "windows.txt"]},
